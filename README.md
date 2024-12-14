cff19bbce7d47dc307cb45ee1b3a496276b19475
tiyst <hello@tiyst.tech>
2024-12-14 03:06:59 +0100

    Color palette and vite config

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+# Development
+
+Project uses React + Sass.
+
+Vite config preconfigured to include color palette in all scss files.
+
+
 # React + TypeScript + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@styles': '/src/styles'
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@styles/colors.scss";`  // Import the colors.scss globally
+      }
+    }
+  }
 })
