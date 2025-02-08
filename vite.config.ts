54ed81df514f3422b2103e09d7ca2d8aef0e200e
tiyst <hello@tiyst.tech>
2025-02-08 03:22:20 +0100

    Vite proxy for be comm
    Prevent CORS errors when running backend and frontend at the same machine

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,27 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	server: {
+		proxy: {
+			'/api': {
+				target: 'http://localhost:8080',
+				changeOrigin: true,
+				// rewrite: (path) => path.replace(/^\/api/, ''),
+
+				configure: (proxy, options) => {
+					proxy.on('error', (err, _req, _res) => {
+						console.log('error', err);
+					});
+					proxy.on('proxyReq', (proxyReq, req, _res) => {
+						console.log('Request sent to target:', req.method, req.url);
+					});
+					proxy.on('proxyRes', (proxyRes, req, _res) => {
+						console.log('Response received from target:', proxyRes.statusCode, req.url);
+					});
+				},
+			},
+		},
+	},
 	resolve: {
 		alias: {
 			'@styles': '/src/styles'
