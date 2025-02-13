2fa50720271d57ae8cb3b7cc6353d1e78245970b
tiyst <hello@tiyst.tech>
2025-02-13 01:59:58 +0100

    Homescreen changes

--- src/screens/homescreen/Homescreen.tsx
+++ src/screens/homescreen/Homescreen.tsx
@@ -1,22 +1,12 @@
-import TopBar from '../../components/topbar/TopBar.tsx';
-import Base from '../../components/base/Base.tsx';
-import { useState } from 'react';
+import { Search } from '../../components/searchBar/Search.tsx';
+import './HomeScreen.scss';
 
 function Homescreen() {
-	const [count, setCount] = useState(0);
 
 	return (
-		<div>
-			<TopBar />
-			<Base>
-				<p>This is test in base</p>
-			</Base>
-			<button>Move to summoner screen</button>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 2)}>count is {count}</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
+		<div className="homescreenContainer">
+			<div className="searchContainer">
+				<Search />
 			</div>
 		</div>
 	);
