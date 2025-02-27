b5331d3590b3f72d8262ebabefa4f31ba60a6313
tiyst <hello@tiyst.tech>
2025-02-28 00:10:29 +0100

    Summoner screen fetching cleanup

--- src/screens/summoner/SummonerScreen.tsx
+++ src/screens/summoner/SummonerScreen.tsx
@@ -6,15 +6,12 @@ import MasteryComponent from '../../components/summoner/mastery/MasteryComponent
 import { MatchComponent } from '../../components/summoner/match/MatchComponent.tsx';
 import { Summoner } from '../../model/Summoner.ts';
 import { useParams } from 'react-router';
-import { useState } from 'react';
 import { Match } from '../../model/Match.ts';
 import { LoadingSpinner } from '../../components/base/LoadingSpinner.tsx';
 import { MissingSummonerFragment } from '../../components/summoner/missingSummoner/missingSummonerFragment.tsx';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 export const SummonerScreen = () => {
-	const [nonExistingSummoner, setNonExistingSummoner] = useState<boolean>(false);
-
 	const queryClient = useQueryClient();
 
 	const { server = 'EUW1', gameName = 'Unknown', tagLine = 'Unknown' } = useParams();
@@ -26,7 +23,6 @@ export const SummonerScreen = () => {
 
 	const updateSummoner = async (): Promise<Summoner> => {
 		const backendUrl = import.meta.env.VITE_BACKEND_URL;
-		setNonExistingSummoner(false);
 		const summoner1 = await pullData(`${backendUrl}/api/summoner/${server}/${gameName}/${tagLine}/update`);
 		console.log(summoner1);
 		return summoner1;
@@ -37,7 +33,6 @@ export const SummonerScreen = () => {
 		if (!res.ok) {
 			const errorData = await res.json();
 			if (res.status === 400) { // Summoner not found
-				setNonExistingSummoner(true);
 			}
 			if (res.status === 425) { // Summoner throttling
 			}
@@ -64,12 +59,10 @@ export const SummonerScreen = () => {
 	});
 
 	if (isFetching || isLoading) {
-		console.log('is in isFetching || isLoading');
 		return <LoadingSpinner />;
 	}
 
-	if (nonExistingSummoner) {
-		console.log('is in nonExistingSummoner || isError');
+	if (!summoner) {
 		return (
 			<>
 				<TopBar />
