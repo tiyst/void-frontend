d6acd81daf2781c720d92f5666033cb79b1c01cc
tiyst <hello@tiyst.tech>
2025-03-17 02:04:10 +0100

    logo for top bar

--- src/components/topbar/TopBar.tsx
+++ src/components/topbar/TopBar.tsx
@@ -1,10 +1,16 @@
 import './TopBar.scss';
 import React from 'react';
 import { Search } from '../searchBar/Search.tsx';
+import { useNavigate } from 'react-router-dom';
 
 const TopBar: React.FC = () => {
+	const navigate = useNavigate();
+
 	return (
 		<div className="top-bar">
+			<button className="logo-button" onClick={() => navigate('/')}>
+				<img src="/void-logo-bar.png" alt="void logo" style={{ maxHeight: '100%', left: '15px' }} />
+			</button>
 			<Search />
 		</div>
 	);
