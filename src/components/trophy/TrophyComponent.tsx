2ce789f13cbc1488f744c9a739926cab633068ee
tiyst <tiystw@gmail.com>
2025-07-23 00:33:30 +0200

    VOID-91 trophy tooltip (#14)
    * VOID-91 Trophy tooltips

--- src/components/trophy/TrophyComponent.tsx
+++ src/components/trophy/TrophyComponent.tsx
@@ -3,6 +3,9 @@ import { findPlayerByPuuid, getMapUrlByMapId } from '../../utils/MatchUtils.ts';
 import { getChampionIconUrl, getUnknownChampionIconUrl, urlUnknownChampion } from '../../utils/IconsUtils.ts';
 import './TrophyComponent.scss';
 import { Trophy } from '../../model/Summoner.ts';
+import { useState } from 'react';
+import { TrophyTooltip } from './TrophyTooltip.tsx';
+import { useIsMobile } from '../../hooks/useIsMobile.ts';
 
 export type TrophyProms = BaseBlockProps & {
 	trophy: Trophy;
@@ -11,10 +14,40 @@ export type TrophyProms = BaseBlockProps & {
 
 export const TrophyComponent = (data: TrophyProms) => {
 	const match = data.trophy.bestMatch ?? null;
+	const isMobile = useIsMobile();
+	const [tooltipPosition, setTooltipPosition] = useState({ x: 0, y: 0, show: false });
+
+	const handleMouseEnter = () => {
+		if (isMobile) return;
+		setTooltipPosition((prev) => ({ ...prev, show: true }));
+	};
+
+	const handleMouseLeave = () => {
+		if (isMobile) return;
+		setTooltipPosition((prev) => ({ ...prev, show: false }));
+	};
+
+	const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+		if (isMobile) return;
+		setTooltipPosition({
+			x: e.clientX,
+			y: e.clientY + 20, // 20px below cursor
+			show: true
+		});
+	};
 
 	if (!match) {
 		return (
-			<div className="trophy-component">
+			<div
+				className="trophy-component"
+				{...(isMobile
+					? {}
+					: {
+							onMouseEnter: handleMouseEnter,
+							onMouseLeave: handleMouseLeave,
+							onMouseMove: handleMouseMove
+						})}
+			>
 				<div className="trophy-icon-wrap">
 					<img
 						src={getUnknownChampionIconUrl()}
@@ -30,6 +63,16 @@ export const TrophyComponent = (data: TrophyProms) => {
 					<div className="trophy-value">??</div>
 					<div className="trophy-date">??/??</div>
 				</div>
+				{!isMobile && (
+					<TrophyTooltip
+						trophy={data.trophy}
+						x={tooltipPosition.x}
+						y={tooltipPosition.y}
+						show={tooltipPosition.show}
+						hasMatch={false}
+						playerGameName={''}
+					/>
+				)}
 			</div>
 		);
 	}
@@ -39,7 +82,16 @@ export const TrophyComponent = (data: TrophyProms) => {
 	const date = new Date(match.gameEndTimestamp);
 
 	return (
-		<div className="trophy-component">
+		<div
+			className="trophy-component"
+			{...(isMobile
+				? {}
+				: {
+						onMouseEnter: handleMouseEnter,
+						onMouseLeave: handleMouseLeave,
+						onMouseMove: handleMouseMove
+					})}
+		>
 			<div className="trophy-icon-wrap">
 				<img className="trophy-map-icon" src={getMapUrlByMapId(match.mapId)} alt="Map icon" draggable={false} />
 				<img
@@ -58,6 +110,16 @@ export const TrophyComponent = (data: TrophyProms) => {
 				</div>
 				<div className="trophy-value">{data.trophy.bestValue}</div>
 			</div>
+			{!isMobile && (
+				<TrophyTooltip
+					trophy={data.trophy}
+					x={tooltipPosition.x}
+					y={tooltipPosition.y}
+					show={tooltipPosition.show}
+					hasMatch={true}
+					playerGameName={player.riotIdGameName}
+				/>
+			)}
 		</div>
 	);
 };
